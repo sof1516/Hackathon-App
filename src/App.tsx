3720e7870452e0f9bb8ba25b5cffe4e8b9ed562e
Abdur Rehman <abdurrehman@Abdurs-MacBook-Air.local>
2024-07-28 06:16:22 +0500

    major changes to UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import "./App.css";
+import { ThemeProvider } from "@/components/theme-provider"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Dashboard } from "./dashboard";
 import { Login } from "./login";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" Component={Login} />
-        <Route path="/dashboard" Component={Dashboard} />
-      </Routes>
-    </Router>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Router>
+        <Routes>
+          <Route path="/" Component={Login} />
+          <Route path="/dashboard" Component={Dashboard} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/dashboard.tsx
+++ src/dashboard.tsx
@@ -82,6 +82,7 @@ export function Dashboard() {
             <Package2 className="h-4 w-4 transition-all group-hover:scale-110" />
             <span className="sr-only">Acme Inc</span>
           </a>
+
           <TooltipProvider>
             <Tooltip>
               <TooltipTrigger asChild>
@@ -96,7 +97,8 @@ export function Dashboard() {
               <TooltipContent side="right">Dashboard</TooltipContent>
             </Tooltip>
           </TooltipProvider>
-          <TooltipProvider>
+
+          {/* <TooltipProvider>
             <Tooltip>
               <TooltipTrigger asChild>
                 <a
@@ -109,8 +111,9 @@ export function Dashboard() {
               </TooltipTrigger>
               <TooltipContent side="right">Orders</TooltipContent>
             </Tooltip>
-          </TooltipProvider>
-          <TooltipProvider>
+          </TooltipProvider> */}
+
+          {/* <TooltipProvider>
             <Tooltip>
               <TooltipTrigger asChild>
                 <a
@@ -123,8 +126,9 @@ export function Dashboard() {
               </TooltipTrigger>
               <TooltipContent side="right">Products</TooltipContent>
             </Tooltip>
-          </TooltipProvider>
-          <TooltipProvider>
+          </TooltipProvider> */}
+
+          {/* <TooltipProvider>
             <Tooltip>
               <TooltipTrigger asChild>
                 <a
@@ -137,7 +141,8 @@ export function Dashboard() {
               </TooltipTrigger>
               <TooltipContent side="right">Customers</TooltipContent>
             </Tooltip>
-          </TooltipProvider>
+          </TooltipProvider> */}
+
           <TooltipProvider>
             <Tooltip>
               <TooltipTrigger asChild>
@@ -153,6 +158,7 @@ export function Dashboard() {
             </Tooltip>
           </TooltipProvider>
         </nav>
+
         <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
           <TooltipProvider>
             <Tooltip>
@@ -169,6 +175,7 @@ export function Dashboard() {
             </Tooltip>
           </TooltipProvider>
         </nav>
+
       </aside>
       <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
         <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
@@ -195,27 +202,27 @@ export function Dashboard() {
                   <Home className="h-5 w-5" />
                   Dashboard
                 </a>
-                <a
+                {/* <a
                   href="#"
                   className="flex items-center gap-4 px-2.5 text-foreground"
                 >
                   <ShoppingCart className="h-5 w-5" />
                   Orders
-                </a>
-                <a
+                </a> */}
+                {/* <a
                   href="#"
                   className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
                 >
                   <Package className="h-5 w-5" />
                   Products
-                </a>
-                <a
+                </a> */}
+                {/* <a
                   href="#"
                   className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
                 >
                   <Users2 className="h-5 w-5" />
                   Customers
-                </a>
+                </a> */}
                 <a
                   href="#"
                   className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
@@ -226,14 +233,15 @@ export function Dashboard() {
               </nav>
             </SheetContent>
           </Sheet>
-          <Breadcrumb className="hidden md:flex">
+          {/* <Breadcrumb className="hidden md:flex">
             <BreadcrumbList>
               <BreadcrumbItem>
                 <BreadcrumbLink asChild>
                   <a href="#">Dashboard</a>
                 </BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator />
+              </BreadcrumbItem> */}
+
+              {/* <BreadcrumbSeparator />
               <BreadcrumbItem>
                 <BreadcrumbLink asChild>
                   <a href="#">Orders</a>
@@ -242,16 +250,19 @@ export function Dashboard() {
               <BreadcrumbSeparator />
               <BreadcrumbItem>
                 <BreadcrumbPage>Recent Orders</BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
+              </BreadcrumbItem> */}
+            {/* </BreadcrumbList>
+          </Breadcrumb> */}
+          <div className="text-3xl">
+            <h1>Dashboard</h1>
+          </div> 
           <div className="relative ml-auto flex-1 md:grow-0">
-            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+            {/* <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
             <Input
               type="search"
               placeholder="Search..."
               className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
-            />
+            /> */}
           </div>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
@@ -279,19 +290,18 @@ export function Dashboard() {
             </DropdownMenuContent>
           </DropdownMenu>
         </header>
-        <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
+        <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-2">
           <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
             <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4">
               <Card className="sm:col-span-2" x-chunk="dashboard-05-chunk-0">
                 <CardHeader className="pb-3">
-                  <CardTitle>Your Orders</CardTitle>
                   <CardDescription className="max-w-lg text-balance leading-relaxed">
-                    Introducing Our Dynamic Orders Dashboard for Seamless
-                    Management and Insightful Analysis.
+                    Total Savings
                   </CardDescription>
+                  <CardTitle>$36,429</CardTitle>
                 </CardHeader>
                 <CardFooter>
-                  <Button>Create New Order</Button>
+                  <Button>Add New Record</Button>
                 </CardFooter>
               </Card>
               <Card x-chunk="dashboard-05-chunk-1">
@@ -560,7 +570,7 @@ export function Dashboard() {
             </Tabs>
           </div>
           <div>
-            <Card className="overflow-hidden" x-chunk="dashboard-05-chunk-4">
+            {/* <Card className="overflow-hidden" x-chunk="dashboard-05-chunk-4">
               <CardHeader className="flex flex-row items-start bg-muted/50">
                 <div className="grid gap-0.5">
                   <CardTitle className="group flex items-center gap-2 text-lg">
@@ -710,7 +720,7 @@ export function Dashboard() {
                   </PaginationContent>
                 </Pagination>
               </CardFooter>
-            </Card>
+            </Card> */}
           </div>
         </main>
       </div>
